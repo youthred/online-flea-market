ea69c722cad220e9537ade9a57b12320ebc655e7
youthred <xxooec@gmail.com>
2020-12-16 20:55:46 +0800

    new project ofm

---
+++ src/main/java/net/add1s/ofm/ofm/OnlineFleaMarketApplication.java
@@ -0,0 +1,13 @@
+package net.add1s.ofm.ofm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFleaMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFleaMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/add1s/ofm/ofm/OnlineFleaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package net.add1s.ofm.ofm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFleaMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
