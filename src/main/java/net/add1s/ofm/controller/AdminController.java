37d3286f47b5a1003265c86e0665a98565f78cfa
youthred <xxooec@gmail.com>
2021-04-07 22:10:09 +0800

    AdminController

---
+++ src/main/java/net/add1s/ofm/controller/AdminController.java
@@ -0,0 +1,12 @@
+package net.add1s.ofm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pj.w@qq.com
+ */
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+}
